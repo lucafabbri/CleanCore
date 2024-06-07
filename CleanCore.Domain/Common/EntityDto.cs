f301b67d6a2219725df1ce1fb16c692e7f317556
Nataraja <lucafabbri84@live.it>
2024-06-07 11:06:34 +0200

    fix: entity dto public property setters

--- CleanCore.Domain/Common/EntityDto.cs
+++ CleanCore.Domain/Common/EntityDto.cs
@@ -1,9 +1,10 @@
-namespace CleanCore.Domain.Common;
+﻿namespace CleanCore.Domain.Common;
 
 /// <summary>
-/// The entity dto interface
+/// The entity dto class
 /// </summary>
-public interface IEntityDto<TId, TEntity, TDto>
+/// <seealso cref="IEntityDto{TId, TEntity, TDto}"/>
+public abstract class EntityDto<TId, TEntity, TDto>:IEntityDto<TId, TEntity, TDto>
     where TId : IEquatable<TId>
     where TEntity : BaseEntity<TId, TEntity, TDto>
     where TDto : IEntityDto<TId, TEntity, TDto>
@@ -11,34 +12,34 @@ public interface IEntityDto<TId, TEntity, TDto>
     /// <summary>
     /// Gets the value of the id
     /// </summary>
-    TId? Id { get; set; }
+    public TId? Id { get; }
     /// <summary>
     /// Gets the value of the created at
     /// </summary>
-    DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset CreatedAt { get; }
     /// <summary>
     /// Gets the value of the created by
     /// </summary>
-    string? CreatedBy { get; set; }
+    public string? CreatedBy { get; }
     /// <summary>
     /// Gets the value of the last modified at
     /// </summary>
-    DateTimeOffset LastModifiedAt { get; set; }
+    public DateTimeOffset LastModifiedAt { get; }
     /// <summary>
     /// Gets the value of the last modified by
     /// </summary>
-    string? LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; }
     /// <summary>
     /// Gets the value of the deleted at
     /// </summary>
-    DateTimeOffset? DeletedAt { get; set; }
+    public DateTimeOffset? DeletedAt { get; }
     /// <summary>
     /// Gets the value of the deleted by
     /// </summary>
-    string? DeletedBy { get; set; }
+    public string? DeletedBy { get; }
     /// <summary>
     /// Returns the entity
     /// </summary>
     /// <returns>The entity</returns>
-    TEntity ToEntity();
+    public abstract TEntity ToEntity();
 }
